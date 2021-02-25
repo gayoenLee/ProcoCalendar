2932ad19f450048b592c3223de881cff244aff9a
gayeon <leeedho67@gmail.com>
2021-02-25 11:39:33 +0900

    [MOD]month view와이어프레임대로 뷰 수정

--- Sources/ProcoCalendar/ProcoCalendar/Views/Monthly/MonthView.swift
+++ Sources/ProcoCalendar/ProcoCalendar/Views/Monthly/MonthView.swift
@@ -5,6 +5,7 @@ import SwiftUI
 struct MonthView: View, MonthlyCalendarManagerDirectAccess {
 
     @Environment(\.calendarTheme) var theme: CalendarTheme
+    @Environment(\.editMode) var editMode
 
     @ObservedObject var calendarManager: MonthlyCalendarManager
 
@@ -25,15 +26,73 @@ struct MonthView: View, MonthlyCalendarManagerDirectAccess {
 
     var body: some View {
         VStack(spacing: 40) {
+            HStack{
             monthYearHeader
                 .padding(.leading, CalendarConstants.Monthly.outerHorizontalPadding)
                 .onTapGesture { self.communicator?.showYearlyView() }
-            weeksViewWithDaysOfWeekHeader
-            if selectedDate != nil {
-                calenderAccessoryView
-                    .padding(.leading, CalendarConstants.Monthly.outerHorizontalPadding)
-                    .id(selectedDate!)
+                /*
+                 case 1.심심기간 설정
+                 - 기간 설정하기: editmode가 true > 완료 editmode가 false
+                 case 2.심심기간 수정
+                 - 기간 수정하기: edit_boring_period가 true..컨텍스트 메뉴에서 true로 변경 > 완료 edit_boring_period가 false
+                 */
+                
+                //기간 설정 모드일 경우
+                if (.active == self.editMode?.wrappedValue){
+                    
+                    Button(action: {
+                        
+                        print("완료 버튼 클릭함.")
+                        self.editMode?.wrappedValue = .active == self.editMode?.wrappedValue ? .inactive : .active
+                        print("전 에딧 모드 값: \(calendarManager.is_edit_mode)")
+                        
+                        calendarManager.is_edit_mode = false
+                    print("후 에딧 모드 값: \(calendarManager.is_edit_mode)")
+                        
+                    }){
+                        Text( "완료")
+                            .font(.callout)
+                    }
+                    
+                //기간 수정 모드일 경우
+                }else if calendarManager.edit_boring_period{
+                    
+                    Button(action: {
+                        
+                        print("수정 완료 클릭")
+                        calendarManager.edit_boring_period = false
+
+                    }){
+                        Text("수정 완료")
+                    }
+                
+                }else {
+                    
+                    Button(action: {
+                        print("추가하기 버튼 클릭함.")
+                        self.editMode?.wrappedValue = .active == self.editMode?.wrappedValue ? .inactive : .active
+                    
+                            print("전 에딧 모드 값: \(calendarManager.is_edit_mode)")
+                            calendarManager.is_edit_mode = true
+                        print("후 에딧 모드 값: \(calendarManager.is_edit_mode)")
+
+                    }){
+                        Text( "기간 설정하기")
+                            .font(.callout)
+                    }
+                }
             }
+                
+            }
+            weeksViewWithDaysOfWeekHeader
+        //일정 리스트 뷰 보여주는 것 예외처리 부분
+        //selectedDate가 존재하면 정보 리스트뷰를 보여준다.
+        if (.inactive == self.editMode?.wrappedValue) && selectedDate != nil{
+            
+            calenderAccessoryView
+                .padding(.leading, CalendarConstants.Monthly.outerHorizontalPadding)
+                .id(selectedDate!)
+        }
             Spacer()
         }
         .padding(.top, CalendarConstants.Monthly.topPadding)
@@ -128,11 +187,72 @@ private struct CalendarAccessoryView: View, MonthlyCalendarManagerDirectAccess {
 
     var body: some View {
         VStack {
+            HStack{
+                
             selectedDayInformationView
-            GeometryReader { geometry in
-                self.datasource?.calendar(viewForSelectedDate: self.selectedDate!,
-                                          dimensions: geometry.size)
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                   print("내 일정 추가하기 버튼 클릭")
+                }){
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width/15, height: UIScreen.main.bounds.width/15)
+                        .foregroundColor(Color.orange)
+                    
+                }
+                .padding(.trailing, UIScreen.main.bounds.width/20)
             }
+            HStack{
+                if calendarManager.selections.contains(selectedDate!){
+                    Button(action: {
+                        print("관심있어요 버튼 클릭")
+                        
+                    }){
+                        Image(systemName: "star")
+                            .resizable()
+                            .frame(width: UIScreen.main.bounds.width/15, height: UIScreen.main.bounds.width/15)
+                            .foregroundColor(Color.yellow)
+                        
+                    }
+                        
+                        Button(action: {
+                            print("관심있어요 갯수 클릭")
+                        }){
+                            Text("관심있어요")
+                                .font(.footnote)
+                        }
+                        Spacer()
+                }else{
+            Button(action: {
+                print("좋아요 버튼 클릭")
+                
+            }){
+                Image(systemName: "heart")
+                    .resizable()
+                    .frame(width: UIScreen.main.bounds.width/15, height: UIScreen.main.bounds.width/15)
+                    .foregroundColor(Color.red)
+                
+            }
+                
+                Button(action: {
+                    print("좋아요 갯수 클릭")
+                }){
+                    Text("좋아요")
+                        .font(.footnote)
+                }
+                Spacer()
+                }
+            }
+           // GeometryReader { geometry in
+                //2.23변경.
+//                self.datasource?.calendar(viewForSelectedDate: calendarManager.selectedDate!,
+//                                          dimensions: UIScreen.main.bounds.size)
+            //}
+            
+            self.datasource?.calendar(viewForScheduleDate: calendarManager.selectedDate!, dimensions: UIScreen.main.bounds.size)
         }
         .onAppear(perform: makeVisible)
         .opacity(isVisible ? 1 : 0)
@@ -158,11 +278,11 @@ private struct CalendarAccessoryView: View, MonthlyCalendarManagerDirectAccess {
     private var dayOfWeekWithMonthAndDayText: some View {
         let monthDayText: String
         if numberOfDaysFromTodayToSelectedDate == -1 {
-            monthDayText = "Yesterday"
+            monthDayText = "어제"
         } else if numberOfDaysFromTodayToSelectedDate == 0 {
-            monthDayText = "Today"
+            monthDayText = "오늘"
         } else if numberOfDaysFromTodayToSelectedDate == 1 {
-            monthDayText = "Tomorrow"
+            monthDayText = "내일"
         } else {
             monthDayText = selectedDate!.dayOfWeekWithMonthAndDay
         }
@@ -174,7 +294,7 @@ private struct CalendarAccessoryView: View, MonthlyCalendarManagerDirectAccess {
 
     private var daysFromTodayText: some View {
         let isBeforeToday = numberOfDaysFromTodayToSelectedDate < 0
-        let daysDescription = isBeforeToday ? "DAYS AGO" : "DAYS FROM TODAY"
+        let daysDescription = isBeforeToday ? "일 전" : "일 후"
 
         return Text("\(abs(numberOfDaysFromTodayToSelectedDate)) \(daysDescription)")
             .font(.system(size: 10))
